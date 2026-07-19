889c09fd8d0975e6e697f43c674551323361ae3b
sre3ya0213 <sreeyatipirisetty0213@gmail.com>
2026-07-19 19:52:32 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length())
+        {
+            return false;
+        }
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for(int i=0;i<s1.length();i++)
+        {
+            s1Count[s1.charAt(i)-'a']++;
+            s2Count[s2.charAt(i)-'a']++;
+        }
+        for(int j=0,i=s1.length();i<=s2.length();i++,j++)
+        {
+            if (Arrays.equals(s1Count, s2Count)) {
+                return true;
+            }
+            if(i < s2.length()) {
+                s2Count[s2.charAt(j)-'a']--;
+                s2Count[s2.charAt(i)-'a']++;    
+            }    
+        }
+        return false;
+        
+    }
+}
